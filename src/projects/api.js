5b6daecda2ff41546c90b31b4dde1832eb53218a
om <kushwahaom331@gmail.com>
2025-07-07 23:47:53 +0530

    sort problem fixed

--- src/projects/api.js
+++ src/projects/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 function getProductList(){
     console.log('get Product Data is running');
     return axios.get("https://dummyjson.com/products").then(function(response){
-        console.log(response);
+        console.log("list:",response);
         return response.data.products;
     });                //this is called promise ,data will fetch from the server till
                                                                       // a token is passed
@@ -11,7 +11,7 @@ function getProductList(){
 }
 export function getProductData(id){
     return axios.get("https://dummyjson.com/products/" + id).then(function(response){
-         console.log(response);
+         console.log("single",response);
          return response.data;
     });
 }
--- src/projects/loading.jsx
+++ src/projects/loading.jsx
@@ -1,9 +1,19 @@
-import {ImSpinner9} from "react-icons/im";
+import { ImSpinner9 } from "react-icons/im";
 
-
-function Loading(){
-    return <div style={{"background-color":"#a6f1f7","font-size":"25px",display:"flex","align-items":"center","justify-content":"center"}}>
-    <ImSpinner9 className="animate-spin  size-8"/>
-    </div>;
+function Loading() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#a6f1f7",
+        fontSize: "25px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ImSpinner9 className="animate-spin size-8" />
+    </div>
+  );
 }
-export default Loading
+
+export default Loading;
